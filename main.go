2dfd19e1c27c10725c99ba3542c7f8bd5eb7c94c
Евгений Накапюк <nameewgeniy@ya.ru>
2020-10-05 21:44:11 +0300

    connect to db

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	conn, err := pgx.Connect(context.Background(), "host=localhost user=point_user password=point_password dbname=point_db_test port=9882")
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var id int64
+
+	err = conn.QueryRow(context.Background(), "SELECT id from ghost_point_orders").Scan(&id)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(id)
 }
