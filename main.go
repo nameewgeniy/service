001d5bab67979aff7f48c453484478fcc67ac013
Евгений Накапюк <e.nakapyuk@adspend.ru>
2020-11-03 19:00:47 +0300

    test

--- main.go
+++ main.go
@@ -8,8 +8,11 @@ import (
 	"github.com/jackc/pgx"
 )
 
+var conn *pgx.Conn
+
 func main() {
-	conn, err := pgx.Connect(context.Background(), "host=localhost user=point_user password=point_password dbname=point_db_test port=9882")
+
+	conn, err := pgx.Connect(context.Background(), "host=localhost user=go_user password=go_password dbname=go_test_db port=9190")
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
@@ -17,13 +20,28 @@ func main() {
 	}
 	defer conn.Close(context.Background())
 
-	var id int64
+	type Data struct {
+		ID      int
+		Doamin  string `json:"domain"`
+		Content string `json:"content"`
+		Date    string `json:"date"`
+	}
 
-	err = conn.QueryRow(context.Background(), "SELECT id from ghost_point_orders").Scan(&id)
+	fmt.Println(result)
+}
+
+func addDomain() error {
+	_, err := conn.Exec(context.Background(), "insert into data(data) values('test')")
+	return err
+}
+
+func getDomain() error {
+
+	var result Data
+
+	err := conn.QueryRow(context.Background(), "SELECT * from data").Scan(&result.ID, &result)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
 		os.Exit(1)
 	}
-
-	fmt.Println(id)
 }
